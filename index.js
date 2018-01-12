b60fa1c1d211ee5cec2f97b525021abad1316614
Sophia Song <sophia.song@tufts.edu>
2018-01-12 18:36:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // write your code below!
-var name = "Susan"
+console.logvar name = "Susan"
 var height = 39
 var message = "${name} is ${height} inches tall"
 module.exports = {name, height, message}
